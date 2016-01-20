1de80dcdc2f54fbbf9387fe9947ee864d023c2a4
Jack Yeh <jack.yeh@gmail.com>
2016-01-20 12:21:32 -0700

    create/pass test: user logs in and is redirected to tasks#index and has tasks from jack

--- test/integration/signin_user_test.rb
+++ test/integration/signin_user_test.rb
@@ -12,5 +12,17 @@ class SignInTest < ActionDispatch::IntegrationTest
 
     assert page.has_content?("Welcome, justin@example.com")
   end
+
+  test "registered user logs in and is redirected to tasks#index view" do
+    user = User.create(email_address: "justin@example.com", password: "password")
+
+    visit login_path
+    fill_in "Email address", with: user.email_address
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    assert page.has_content?("Tasks by Jack")
+    assert_current_path(tasks_path)
+  end
 end
 
