b04f8f6e33c7c0ceeae00e834162f5c96b058edd
Jack Yeh <jack.yeh@gmail.com>
2016-01-20 18:24:09 -0700

    add functionality: can edit tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,10 @@ class TasksController < ApplicationController
     @lists = TasksList.all
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     list = TasksList.find(params[:tasks_list_id])
     @task = list.tasks.create(task_params)
@@ -15,6 +19,16 @@ class TasksController < ApplicationController
       redirect_to tasks_path
     else
       render :new
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      flash[:alert] = "Invalid Title"
+      render :edit
     end
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
 class Task < ActiveRecord::Base
   belongs_to :tasks_lists
+
+  def self.past_due
+   past_due = []
+   tasks = Task.all
+   tasks.each do |task|
+   require "pry"; binding.pry
+     if Date.new > task.due
+       past_due.push(task)
+     end
+   end
+   past_due
+  end
 end
